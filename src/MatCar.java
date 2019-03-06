ae98a3e7ac08afde0ac5974861f8b8658f9361a4
Nicolas-62 <droffig@hotmail.com>
2019-03-06 23:04:35 +0100

    calcul determinant matrice ordre N

---
+++ src/MatCar.java
@@ -0,0 +1,57 @@
+
+public final class MatCar {
+	public int ordre;
+	public Double[][] mat;
+
+	public MatCar() {
+	}
+
+	public MatCar(int unOrdre) {
+		this.ordre = unOrdre;
+		this.mat = new Double[this.ordre][this.ordre];
+	}
+
+	public MatCar(int unOrdre, Double[][] uneMat) {
+		this.ordre = unOrdre;
+		this.mat = uneMat;
+	}
+
+	public double calculerDet() {
+		double val = 0;
+		if (this.ordre == 2) {
+			return this.calculerDetMatOrdre2();
+		} else if (this.ordre > 2) {
+			int indiceMatSup = 0;
+			int operateur = 0;
+			// création des matrices d'ordre inferieur
+			for (int i = 0; i < this.ordre; i++) {
+				MatCar uneMatCar = new MatCar(ordre - 1);
+				// remplissage des lignes
+				for (int j = 0; j < uneMatCar.ordre; j++) {
+					// remplissage des colonnes sauf celle qui vaut i
+					indiceMatSup = 0;
+					for (int k = 0; k < uneMatCar.ordre; k++) {
+						// on saute la colonne d'indice i
+						if (k == i && i < uneMatCar.ordre) {
+							indiceMatSup++;
+						}
+						uneMatCar.mat[j][k] = this.mat[j + 1][indiceMatSup];
+						indiceMatSup++;
+					}
+				}
+				if (i % 2 == 0) {
+					operateur = 1;
+				} else {
+					operateur = -1;
+				}
+				val += this.mat[0][i] * operateur * uneMatCar.calculerDet();
+			}
+		}
+		return val;
+	}
+
+	public double calculerDetMatOrdre2() {
+		return (this.mat[0][0] * this.mat[1][1]) - (this.mat[1][0] * this.mat[0][1]);
+	}
+
+}
---
+++ src/TestMatrice.java
@@ -0,0 +1,23 @@
+
+
+public class TestMatrice {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner sc = new Scanner(System.in);
+//		Double[][] matrice;
+//		System.out.println("veuillez saisir l'ordre de votre matrice carrée : ");
+//		int ordre = sc.nextInt();
+		Double[][] mat = {{1d,2d},{3d,4d}};
+		MatCar uneMatCar = new MatCar(2, mat);
+		System.out.println("resultat : "+uneMatCar.calculerDet());
+		
+		Double[][] mat2 = {{3d,2d,1d},{9d,3d,1d},{36d,6d,1d}};
+		MatCar uneMatCar2 = new MatCar(3, mat2);
+		System.out.println("resultat 2 : "+uneMatCar2.calculerDet());
+		
+		Double[][] mat3 = {{3d,0d,2d,-1d},{1d,2d,0d,-2d},{4d,0d,6d,-3d},{5d,0d,2d,0d}};
+		MatCar uneMatCar3 = new MatCar(3, mat3);
+		System.out.println("resultat 3 : "+uneMatCar3.calculerDet());
+	}
+
+}

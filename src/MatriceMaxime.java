6c7e2f93ca0b27f968d0ccec868c57ce17332d29
sario <sario@LAPTOP-BTS8QBF7>
2019-03-19 16:56:57 +0100

    Affichage de la transposée (Maxime)

--- src/MatriceMaxime.java
+++ src/MatriceMaxime.java
@@ -15,6 +15,18 @@ public class MatriceMaxime {
 		System.out.println("|"+mat[1][0]+" "+mat[1][1]+"|");
 		System.out.println("|"+mat[2][0]+" "+mat[2][1]+"|");
 		
+		System.out.println("------------------------------------------------------");
+		
+		Double[][] mat2 = {{1d,2d,3d},{4d,5d,6d}};
+		System.out.println("|"+mat2[0][0]+" "+mat2[0][1]+" "+mat2[0][2]+"|");
+		System.out.println("|"+mat2[1][0]+" "+mat2[1][1]+" "+mat2[1][2]+"|");
+		
+		System.out.println("-----------------------------------------------------");
+		
+		Double[][] mat3 = {{1d,4d},{2d,5d},{3d,6d}};
+		System.out.println("|"+mat3[0][0]+" "+mat3[0][1]+"|");
+		System.out.println("|"+mat3[1][0]+" "+mat3[1][1]+"|");
+		System.out.println("|"+mat3[2][0]+" "+mat3[2][1]+"|");
 	}
 
 }
